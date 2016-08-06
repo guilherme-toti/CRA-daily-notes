53e64518afd2c5eb26d8f8628974d7ad481b3bc9
Guilherme Toti <guilhermetoti@gmail.com>
2016-08-05 21:59:58 -0300

    first files

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+
+class NoteItem extends Component {
+
+  render() {
+    const { item, handleRemove } = this.props;
+
+    return (
+      <li>
+        <span className="notes__item">{item.id} - { item.value }</span>
+        <span
+          className="notes__delete-button"
+          onClick={(e) => {
+            e.preventDefault();
+            handleRemove(item.id);
+          }}>x
+        </span>
+      </li>
+    );
+  }
+}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+     notes: [],
+     text: '',
+     loading: false,
+    };
+  }
+
+  handleTextChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const value = this.state.text;
+
+    if (! value.trim()) {
+      return false;
+    }
+
+    this.setState({
+      notes: this.state.notes.concat({
+        id: this.state.notes.length + 1,
+        value: value
+      }),
+      text: ''
+    })
+  }
+
+  removeItem(id) {
+    const notes = this.state.notes;
+
+    this.setState({
+      notes: notes.filter((item) => {
+        return item.id !== id;
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="header">
+          <h2>My daily notes</h2>
+          <form
+            className="form-header"
+            onSubmit={this.handleSubmit.bind(this)}>
+            <input
+              type="text"
+              className="form-header__input"
+              value={this.state.text}
+              onChange={this.handleTextChange.bind(this)} />
+            {/*<img src={logo} className="form-header__icon-loading" alt="loading" />*/}
+          </form>
+        </div>
+        <ul className="notes">
+          {this.state.notes.map((obj, i) => {
+            return <NoteItem
+                    item={obj}
+                    key={i}
+                    handleRemove={this.removeItem.bind(this)} />
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
